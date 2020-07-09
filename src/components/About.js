32cd48a3f6a983344cf874c52dd7060609797734
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-07-08 18:39:46 -0700

    updating

--- src/components/About.js
+++ src/components/About.js
@@ -1,37 +1,36 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const AboutSection = styled.div`
-
-    display: block;
-    width: 50%;
-    border: 2px solid navy;
-    border-radius: 5rem;
-    padding: 1%;
-    background-color: skyblue;
-    box-shadow: 0 0 2rem 0 dodgerblue;
-
-`
+  display: block;
+  width: 50%;
+  border: 2px solid navy;
+  border-radius: 5rem;
+  padding: 1%;
+  background-color: skyblue;
+  box-shadow: 0 0 2rem 0 dodgerblue;
+`;
 
 const ProfilePic = styled.img`
-
-    border: 2px solid navy;
-    border-radius: 5rem;
-    width: 30rem;
-
-`
+  border: 2px solid navy;
+  border-radius: 5rem;
+  width: 30rem;
+`;
 
 const AboutMe = styled.p`
-
-    margin: 3%;
-
-`
+  margin: 3%;
+`;
 
 export default function About() {
-    return (
-        <AboutSection>
-            <ProfilePic src={require("../img/me.jpg")} alt="profile" />
-            <AboutMe>My name is Nick, I am 33 years old, and I live in Portland, OR. I love to learn, especially about technology. I've been an avid gamer and computer enthusiast since I was young. I like to build my own computers and beatbox using a RC-505 loop station as hobbies.</AboutMe>
-        </AboutSection>
-    )
+  return (
+    <AboutSection>
+      <ProfilePic src={require("../img/me.jpg")} alt="profile" />
+      <AboutMe>
+        My name is Nick, I am 33 years old, and I live in Portland, OR. I love
+        to learn, especially about technology. I've been an avid gamer and
+        computer enthusiast since I was young. I like to build my own computers
+        and beatbox using a RC-505 loop station as hobbies.
+      </AboutMe>
+    </AboutSection>
+  );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,13 +7,15 @@ const ProjectCard = styled.div`
   flex-flow: column;
   justify-content: space-around;
   background-color: dodgerblue;
-  height: 60vh;
+  height: 70vh;
   padding: 2rem;
   border-radius: 1rem;
 
-  p, h2 {
-      color: lightblue;
-      text-shadow:  0 0 1rem navy;
+  p,
+  h2 {
+    color: lightblue;
+    text-shadow: 0 0 1rem navy;
+    margin: 3%;
   }
 `;
 
@@ -21,8 +23,12 @@ export default function Projects() {
   return (
     <>
       <ProjectCard>
+       
         <h2>The Family Scrapbook</h2>
-        <iframe src="https://the-family-scrapbook.netlify.app/starthere"></iframe>
+        <iframe
+          src="https://the-family-scrapbook.netlify.app/starthere"
+          height="100%"
+        ></iframe>
         <p>
           I worked on this website as part of a 3 person team. My responsibility
           was the "Start Here Page". I also helped in the design and
@@ -32,9 +38,17 @@ export default function Projects() {
       </ProjectCard>
 
       <ProjectCard>
+        
         <h2>GEARformation: Division 2</h2>
-        <iframe src="https://gearformation.netlify.app/"></iframe>
-        <p>This site is a personal project. Division 2 is a video game I play. The information for the game was spread out over many sites or spreadsheets found online. My goal was to compile that information into one site to make the information easier to find. The next steps for this project are to convert to JSON and implement it as an API that anyone could use. Currently the code is written in HTML and CSS.</p>
+        <iframe src="https://gearformation.netlify.app/" height="100%"></iframe>
+        <p>
+          This is a personal project. The information for the game was spread
+          out over many sites or spreadsheets found online. My goal was to
+          compile that information into one site to make the information easier
+          to find. The next steps for this project are to convert to JSON and
+          implement it as an API that anyone could use. Currently the code is
+          written in HTML and CSS.
+        </p>
       </ProjectCard>
 
       <ProjectCard>
