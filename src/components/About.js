4e8d05f13cc1ecad95b20348d5288d1387bb3a86
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-07-12 21:05:04 -0700

    fixed about positioning

--- src/components/About.js
+++ src/components/About.js
@@ -6,24 +6,32 @@ const AboutSection = styled.div`
   width: 50%;
   border-radius: 5rem;
   padding: 1%;
-  background-color: #5A86AF;
-  box-shadow: 0 0 2rem 0 #3ABAE6;
+  background-color: #5a86af;
+  box-shadow: 0 0 2rem 0 #3abae6;
   display: flex;
   justify-content: space-around;
   flex-flow: column;
   align-items: center;
+  * {
+    /* border: 1px solid black; */
+  }
 `;
 
 const ProfilePic = styled.img`
   // border: 2px solid #122059;
   border-radius: 5rem;
   height: 50%;
+  /* width: 50% */
 `;
 
 const AboutMe = styled.p`
+  display: flex;
+  align-items: center;
+  border-radius: 2rem;
   font-size: 2rem;
-  color: #D5E3EB;
-  text-shadow: 0 2px 20px #1A2243;
+  color: #d5e3eb;
+  text-shadow: 0 2px 20px #1a2243;
+  height: 50%;
 `;
 
 export default function About() {
